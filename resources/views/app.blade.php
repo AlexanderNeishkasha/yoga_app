d1b3043dc046a16acf0c0bf6b3de47c1fa00c73e
Alexander <neishkasha.alexandr@gmail.com>
2019-02-12 09:10:44 +0200

    add logo icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <title>{{ config('app.name') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="theme-color" content="#ff9800">
+        <link rel="icon" sizes="192x192" href="{{ asset('img/logo_icon_for_tab.png') }}">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
