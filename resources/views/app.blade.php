e516b447a7b873a2953e4a9e31030908bb54d67e
Alexander <neishkasha.alexandr@gmail.com>
2019-02-12 09:05:26 +0200

    fix title tag for app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,8 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <title>{{ config('app.name') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>YogaApp</title>
+        <meta name="theme-color" content="#ff9800">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
